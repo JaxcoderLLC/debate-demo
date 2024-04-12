75e501886c3c29a82dceaa57368b08efc29c9803
Jaxcoder <codenamejason@outlook.com>
2024-04-12 17:17:52 -0400

    more clean up

--- src/app/community/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import CommunityDetail from "@/components/commuinity/CommunityDetail";
-
-const CommunityDetailPage = () => {
-  return <CommunityDetail />;
-};
-
-export default CommunityDetailPage;
--- src/app/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-const SettingsPage = () => {
-  return (
-    <div className="">
-      <div className="text-xl">Settings</div>
-    </div>
-  );
-};
-
-export default SettingsPage;
