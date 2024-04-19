135d74a89e6f614d739dc01d0d4eb32bf038ccc5
Jaxcoder <codenamejason@outlook.com>
2024-04-19 17:07:03 -0400

    update layout

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -13,6 +13,7 @@ import RichardMImage from "../../assets/candidates/RichardM.png";
 import JohnSImage from "../../assets/candidates/JohnS.png";
 import Image from "next/image";
 import { useState } from "react";
+import { CurrencyDollarIcon } from "@heroicons/react/20/solid";
 
 /* eslint-disable @next/next/no-img-element */
 const Donate = () => {
@@ -64,59 +65,64 @@ function Candidates() {
       <div className="mx-auto max-w-screen">
         <ul
           role="list"
-          className="mx-auto mt-4 grid max-w-screen grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:grid-cols-1"
+          className="mx-auto mt-4 grid max-w-screen grid-cols-1 gap-x-8 gap-y-10 lg:mx-0"
         >
           {candidates.map((person) => (
-            <li key={person.name} className="flex flex-row">
-              <div className="flex">
-                {/* Candidate Avatar */}
-                <Image
-                  className="aspect-[3/2] w-full object-cover"
-                  src={person.imageUrl}
-                  alt="Candidate Avatar"
-                  width={200}
-                  height={200}
-                />
-              </div>
-              <div className="flex">
-                <h3 className="mb-2 text-center text-lg font-semibold leading-8 tracking-tight text-gray-900">
-                  {person.name}
-                </h3>
-              </div>
+            <li key={person.name} className="flex flex-row ">
+              <div className="flex flex-row">
+                <div className="relative w-40 md:h-56 h-40">
+                  <Image
+                    src={person.imageUrl}
+                    alt={`${person.name} image`}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-lg"
+                  />
+                </div>
+                <div className="flex flex-col justify-between ml-4 md:ml-12 mr-6">
+                  <ul className="flex flex-row md:mt-10 mt-6 gap-4">
+                    <li key={person.id}>
+                      <DonateButton amount={5} index={person.id} />
+                    </li>
+                    <li key={person.id}>
+                      <DonateButton amount={10} index={person.id} />
+                    </li>
+                    <li key={person.id}>
+                      <DonateButton amount={20} index={person.id} />
+                    </li>
+                  </ul>
+                  <h3 className="text-2xl font-bold md:mb-14 mb:8 text-center">
+                    {person.name}
+                  </h3>
+                </div>
 
-              <input
-                className="mt-4 p-2 px-3 border rounded-lg text-sm text-gray-700"
-                placeholder="Enter amount to donate"
-                onChange={(e) =>
-                  setCustomAmount({
-                    ...customAmount,
-                    [person.id]: { amount: Number(e.target.value) },
-                  })
-                }
-                type="number"
-              />
-              <div className="flex">
-                {/* Donate Buttons */}
-                <ul
-                  role="row"
-                  className="mt-2 flex flex-row justify-between gap-1"
-                >
-                  <li>
-                    <DonateButton amount={5} index={person.id} />
-                  </li>
-                  <li>
-                    <DonateButton amount={25} index={person.id} />
-                  </li>
-                  <li>
-                    <DonateButton amount={50} index={person.id} />
-                  </li>
-                  <li>
-                    <DonateButton
-                      amount={customAmount[person.id].amount}
-                      index={person.id}
-                    />
-                  </li>
-                </ul>
+                <div className="flex flex-col gap-4 md:mt-12 mt-8 mr-4">
+                  <div>
+                    <div className="relative mt-2 rounded-md shadow-sm mb-4">
+                      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                        <CurrencyDollarIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <input
+                        type="number"
+                        placeholder="Enter amount"
+                        onChange={(e) => {
+                          setCustomAmount({
+                            ...customAmount,
+                            [person.id]: { amount: parseInt(e.target.value) },
+                          });
+                        }}
+                        className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                  <DonateButton
+                    amount={customAmount[person.id]?.amount}
+                    index={person.id}
+                  />
+                </div>
               </div>
             </li>
           ))}
