3409b620d5cb0d52006f504f030d1853bc9acbc4
Sean M. Gonzalez <sean.moore.gonzalez@gmail.com>
2024-05-10 15:00:28 -0700

    added chart js

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import * as d3 from 'd3';
+import { useEffect, useLayoutEffect, useState } from "react";
+
 export default function Live() {
+
+  const margin = { top: 10, right: 30, bottom: 30, left: 60 },
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+  // append the svg object to the body of the page
+  const svg = d3.select("#my_dataviz")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
   return (
     <div>
       <h1>Live Page</h1>
+      <div id="my_dataviz" />
     </div>
   );
 }
