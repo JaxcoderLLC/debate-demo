c26e17d3ebf5f6c8b301b34fd89d66e534e53e19
Jaxcoder <codenamejason@outlook.com>
2024-05-10 12:51:49 -0400

    profile page/add forms

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,14 +1,80 @@
 "use client";
 
+import { Fragment, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import PhotoIcon from "@heroicons/react/20/solid/PhotoIcon";
+import UserCircleIcon from "@heroicons/react/24/outline/UserCircleIcon";
+import { classNames } from "@/utils/common";
+import { Menu, Transition } from "@headlessui/react";
+import {
+  CursorArrowRaysIcon,
+  EllipsisVerticalIcon,
+  EnvelopeOpenIcon,
+  UsersIcon,
+} from "@heroicons/react/20/solid";
+import ArrowUpIcon from "@heroicons/react/24/outline/ArrowUpIcon";
+import ArrowDownIcon from "@heroicons/react/24/outline/ArrowDownIcon";
 
 const schema = yup.object({
   roundName: yup.string().required(),
   roundStart: yup.date().required(),
   roundEnd: yup.date().required(),
 });
+
+const tabs = [
+  { name: "Profile", current: true },
+  { name: "Events", current: false },
+  { name: "Stats", current: false },
+  { name: "Team Members", current: false },
+  { name: "Billing", current: false },
+];
+
+function Tabs({
+  activeTab,
+  setActiveTab,
+}: {
+  activeTab: string;
+  setActiveTab: (tab: string) => void;
+}) {
+  return (
+    <div>
+      <nav
+        className="isolate flex divide-x divide-gray-200 rounded-t-lg shadow"
+        aria-label="Tabs"
+      >
+        {tabs.map((tab, tabIdx) => (
+          <span
+            key={tab.name}
+            onClick={(e) => {
+              e.preventDefault();
+              setActiveTab(tab.name);
+            }}
+            className={classNames(
+              tab.name === activeTab
+                ? "text-gray-900"
+                : "text-gray-500 hover:text-gray-700",
+              tabIdx === 0 ? "rounded-l-lg" : "",
+              tabIdx === tabs.length - 1 ? "rounded-r-lg" : "",
+              "group cursor-pointer relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
+            )}
+            aria-current={tab.name === activeTab ? "page" : undefined}
+          >
+            <span>{tab.name}</span>
+            <span
+              aria-hidden="true"
+              className={classNames(
+                tab.name === activeTab ? "bg-indigo-500" : "bg-transparent",
+                "absolute inset-x-0 bottom-0 h-0.5"
+              )}
+            />
+          </span>
+        ))}
+      </nav>
+    </div>
+  );
+}
 
 export default function Manage() {
   const {
@@ -17,13 +83,435 @@ export default function Manage() {
     watch,
     formState: { errors },
   } = useForm();
+  const [activeTab, setActiveTab] = useState("Profile");
   const onSubmit = (data: any) => console.log(data);
-
-  
 
   return (
     <div className="mt-8">
-      <h1>Manage Page</h1>
+      <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === "Profile" && (
+        <ProfileForm
+          register={register}
+          handleSubmit={handleSubmit}
+          onSubmit={onSubmit}
+        />
+      )}
+      {activeTab === "Events" && <EventList />}
+      {activeTab === "Stats" && <Stats />}
+      {/* Similarly, add other forms for other tabs as needed */}
+    </div>
+  );
+}
+
+function ProfileForm({ register, handleSubmit, onSubmit }) {
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-b-xl md:col-span-2 p-2"
+    >
+      <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
+        <div>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Profile
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            This information will be displayed publicly so be careful what you
+            share.
+          </p>
+        </div>
+
+        <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
+          <div className="sm:col-span-4">
+            <label
+              htmlFor="profileName"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Profile Name
+            </label>
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  type="text"
+                  name="profileName"
+                  id="profileName"
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="My Cool Profile"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="sm:col-span-4">
+            <label
+              htmlFor="website"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Website
+            </label>
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
+                  http://
+                </span>
+                <input
+                  type="text"
+                  name="website"
+                  id="website"
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="www.example.com"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-full">
+            <label
+              htmlFor="about"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              About
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="about"
+                name="about"
+                placeholder="I'm a cool person who likes to do cool things."
+                rows={3}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={""}
+              />
+            </div>
+            <p className="mt-3 text-sm leading-6 text-gray-600">
+              Write a few sentences about yourself.
+            </p>
+          </div>
+
+          <div className="col-span-full">
+            <label
+              htmlFor="photo"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Photo
+            </label>
+            <div className="mt-2 flex items-center gap-x-3">
+              <UserCircleIcon
+                className="h-12 w-12 text-gray-300"
+                aria-hidden="true"
+              />
+              <button
+                type="button"
+                className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              >
+                Change
+              </button>
+            </div>
+          </div>
+
+          <div className="col-span-full">
+            <label
+              htmlFor="cover-photo"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Cover photo
+            </label>
+            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+              <div className="text-center">
+                <PhotoIcon
+                  className="mx-auto h-12 w-12 text-gray-300"
+                  aria-hidden="true"
+                />
+                <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Upload a file</span>
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      type="file"
+                      className="sr-only"
+                    />
+                  </label>
+                  <p className="pl-1">or drag and drop</p>
+                </div>
+                <p className="text-xs leading-5 text-gray-600">
+                  PNG, JPG, GIF up to 10MB
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="button"
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
+}
+
+const statuses = {
+  Complete: "text-green-700 bg-green-50 ring-green-600/20",
+  "In progress": "text-gray-600 bg-gray-50 ring-gray-500/10",
+  Archived: "text-yellow-800 bg-yellow-50 ring-yellow-600/20",
+};
+const projects = [
+  {
+    id: 1,
+    name: "GraphQL API",
+    href: "#",
+    status: "Complete",
+    createdBy: "Leslie Alexander",
+    dueDate: "March 17, 2023",
+    dueDateTime: "2023-03-17T00:00Z",
+  },
+  {
+    id: 2,
+    name: "New benefits plan",
+    href: "#",
+    status: "In progress",
+    createdBy: "Leslie Alexander",
+    dueDate: "May 5, 2023",
+    dueDateTime: "2023-05-05T00:00Z",
+  },
+  {
+    id: 3,
+    name: "Onboarding emails",
+    href: "#",
+    status: "In progress",
+    createdBy: "Courtney Henry",
+    dueDate: "May 25, 2023",
+    dueDateTime: "2023-05-25T00:00Z",
+  },
+  {
+    id: 4,
+    name: "iOS app",
+    href: "#",
+    status: "In progress",
+    createdBy: "Leonard Krasner",
+    dueDate: "June 7, 2023",
+    dueDateTime: "2023-06-07T00:00Z",
+  },
+  {
+    id: 5,
+    name: "Marketing site redesign",
+    href: "#",
+    status: "Archived",
+    createdBy: "Courtney Henry",
+    dueDate: "June 10, 2023",
+    dueDateTime: "2023-06-10T00:00Z",
+  },
+];
+
+function EventList() {
+  return (
+    <ul role="list" className="divide-y divide-gray-100">
+      {projects.map((project) => (
+        <li
+          key={project.id}
+          className="flex items-center justify-between gap-x-6 py-5"
+        >
+          <div className="min-w-0">
+            <div className="flex items-start gap-x-3">
+              <p className="text-sm font-semibold leading-6 text-gray-900">
+                {project.name}
+              </p>
+              <p
+                className={classNames(
+                  statuses[project.status],
+                  "rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset"
+                )}
+              >
+                {project.status}
+              </p>
+            </div>
+            <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+              <p className="whitespace-nowrap">
+                Due on{" "}
+                <time dateTime={project.dueDateTime}>{project.dueDate}</time>
+              </p>
+              <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
+                <circle cx={1} cy={1} r={1} />
+              </svg>
+              <p className="truncate">Created by {project.createdBy}</p>
+            </div>
+          </div>
+          <div className="flex flex-none items-center gap-x-4">
+            <a
+              href={project.href}
+              className="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
+            >
+              View project<span className="sr-only">, {project.name}</span>
+            </a>
+            <Menu as="div" className="relative flex-none">
+              <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+                <span className="sr-only">Open options</span>
+                <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
+              </Menu.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          active ? "bg-gray-50" : "",
+                          "block px-3 py-1 text-sm leading-6 text-gray-900"
+                        )}
+                      >
+                        Edit<span className="sr-only">, {project.name}</span>
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          active ? "bg-gray-50" : "",
+                          "block px-3 py-1 text-sm leading-6 text-gray-900"
+                        )}
+                      >
+                        Move<span className="sr-only">, {project.name}</span>
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          active ? "bg-gray-50" : "",
+                          "block px-3 py-1 text-sm leading-6 text-gray-900"
+                        )}
+                      >
+                        Delete<span className="sr-only">, {project.name}</span>
+                      </a>
+                    )}
+                  </Menu.Item>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+const stats = [
+  {
+    id: 1,
+    name: "Total Subscribers",
+    stat: "71,897",
+    icon: UsersIcon,
+    change: "122",
+    changeType: "increase",
+  },
+  {
+    id: 2,
+    name: "Avg. Open Rate",
+    stat: "58.16%",
+    icon: EnvelopeOpenIcon,
+    change: "5.4%",
+    changeType: "increase",
+  },
+  {
+    id: 3,
+    name: "Avg. Click Rate",
+    stat: "24.57%",
+    icon: CursorArrowRaysIcon,
+    change: "3.2%",
+    changeType: "decrease",
+  },
+];
+
+function Stats() {
+  return (
+    <div className="mt-2">
+      <h3 className="text-base font-semibold leading-6 text-gray-900">
+        Last 30 days
+      </h3>
+
+      <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+        {stats.map((item) => (
+          <div
+            key={item.id}
+            className="relative overflow-hidden rounded-lg bg-white px-4 pb-12 pt-5 shadow sm:px-6 sm:pt-6"
+          >
+            <dt>
+              <div className="absolute rounded-md bg-indigo-500 p-3">
+                <item.icon className="h-6 w-6 text-white" aria-hidden="true" />
+              </div>
+              <p className="ml-16 truncate text-sm font-medium text-gray-500">
+                {item.name}
+              </p>
+            </dt>
+            <dd className="ml-16 flex items-baseline pb-6 sm:pb-7">
+              <p className="text-2xl font-semibold text-gray-900">
+                {item.stat}
+              </p>
+              <p
+                className={classNames(
+                  item.changeType === "increase"
+                    ? "text-green-600"
+                    : "text-red-600",
+                  "ml-2 flex items-baseline text-sm font-semibold"
+                )}
+              >
+                {item.changeType === "increase" ? (
+                  <ArrowUpIcon
+                    className="h-5 w-5 flex-shrink-0 self-center text-green-500"
+                    aria-hidden="true"
+                  />
+                ) : (
+                  <ArrowDownIcon
+                    className="h-5 w-5 flex-shrink-0 self-center text-red-500"
+                    aria-hidden="true"
+                  />
+                )}
+
+                <span className="sr-only">
+                  {" "}
+                  {item.changeType === "increase"
+                    ? "Increased"
+                    : "Decreased"}{" "}
+                  by{" "}
+                </span>
+                {item.change}
+              </p>
+              <div className="absolute inset-x-0 bottom-0 bg-gray-50 px-4 py-4 sm:px-6">
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    View all<span className="sr-only"> {item.name} stats</span>
+                  </a>
+                </div>
+              </div>
+            </dd>
+          </div>
+        ))}
+      </dl>
     </div>
   );
 }
